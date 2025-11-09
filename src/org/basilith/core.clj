ae026b066f42781b81764a7c2b502840b152b38e
Stef Gijsberts <stef@stef.link>
2025-11-09 22:16:43 +0100

    Make audio thread loop

--- src/org/basilith/core.clj
+++ src/org/basilith/core.clj
@@ -1,12 +1,13 @@
 (ns org.basilith.core
-    (:import [java.nio ByteBuffer ByteOrder])
+  (:import [java.nio ByteBuffer ByteOrder])
   (:require
    [clojure.java.process :as p]
    [clojure.math :refer [PI sin]]))
 
 (def sample-rate-hz 44000)
+(def chunk-size (/ sample-rate-hz 16))
 
-(def aplay (p/start "aplay" "-f" "FLOAT_LE" "-r" (str sample-rate-hz)))
+(def aplay (p/start "aplay" "--buffer-size" (str chunk-size) "-f" "FLOAT_LE" "-r" (str sample-rate-hz)))
 
 (def aplay-stdin (p/stdin aplay))
 
@@ -17,17 +18,21 @@
   (swap! settings assoc :frequency-hz (+ 400 (rand-int 200)))
   (swap! settings assoc :gain-factor (rand)))
 
-(def chunk-size (/ sample-rate-hz 4))
-
 (def buffer (ByteBuffer/allocate (* 4 chunk-size)))
 (.order buffer ByteOrder/LITTLE_ENDIAN)
 
-(future
-  (dotimes [chunk-index 20]
-    (let [s @settings]
-      (.clear buffer)
-      (dotimes [i chunk-size]
-        (let [sample-index (+ (* chunk-index chunk-size) i)
-              t-seconds (/ sample-index sample-rate-hz)]
-          (.putFloat buffer (* (:gain-factor s) (sin (* 2.0 PI (:frequency-hz s) t-seconds))))))
-      (.write aplay-stdin (.array buffer)))))
+(def audio
+  (future
+    (loop [chunk-index 0]
+      (let [s @settings]
+        (.clear buffer)
+        (dotimes [i chunk-size]
+          (let [sample-index (+ (* chunk-index chunk-size) i)
+                t-seconds (/ sample-index sample-rate-hz)]
+            (.putFloat buffer (* (:gain-factor s) (sin (* 2.0 PI (:frequency-hz s) t-seconds))))))
+        (.write aplay-stdin (.array buffer))
+        (when-not (Thread/interrupted)
+          (recur (inc chunk-index)))))))
+
+(comment
+  (future-cancel audio))
